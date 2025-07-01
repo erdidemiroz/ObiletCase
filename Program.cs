05e8f3a3e0ee67df48523ba9dea21f5d99901e75
erdi.demiroz <erdi.demiroz@gmail.com>
2025-07-01 21:19:36 +0300

    obilet api service and configurations are added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using ObiletCase.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("ObiletClient", client =>
+{
+    client.BaseAddress = new Uri("https://v2-api.obilet.com/api");
+});
+
+builder.Services.AddScoped<ObiletApiService>();
 
 var app = builder.Build();
 
@@ -9,13 +18,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
+// Default Route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
---
+++ Services/ObiletApiService.cs
@@ -0,0 +1,29 @@
+﻿namespace ObiletCase.Services
+{
+    public class ObiletApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ObiletApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("ObiletClient");
+        }
+
+        public async Task<string> GetSessionAsync()
+        {
+            var requestBody = new
+            {
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("/client/getsession", requestBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return result; 
+            }
+
+            return String.Empty;
+        }
+    }
+}
